13192222a6a0ff9a40d61370eaf1cdf4a6b59fb5
Đào Ngọc Thạch <94102125+Daongocthach@users.noreply.github.com>
2026-09-11 07:33:36 +0700

    Fix layout source parsing

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,3 @@
-export default function RootLayout({children}){return <html lang="vi"><body>{children}</body></html>}
+export default function RootLayout({children}) {
+  return <html lang="vi"><body>{children}</body></html>
+}
