5fda6979ab9100a546745ced6d88a4b9089e43c1
Đào Ngọc Thạch <94102125+Daongocthach@users.noreply.github.com>
2026-09-13 20:05:39 +0700

    Enable finance transaction pagination

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import './globals.css'
 import './lifeos-polish.css'
 import MoneyInputFormatter from './money-input-formatter'
+import TablePagination from './table-pagination'
 
 export default function RootLayout({children}) {
-  return <html lang="vi"><body><MoneyInputFormatter />{children}</body></html>
+  return <html lang="vi"><body><MoneyInputFormatter /><TablePagination />{children}</body></html>
 }
