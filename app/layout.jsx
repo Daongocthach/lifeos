758973f2a802462ead721fe1245ad37d8de6b72f
Đào Ngọc Thạch <94102125+Daongocthach@users.noreply.github.com>
2026-09-13 20:01:58 +0700

    Enable formatted money inputs

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import './lifeos-polish.css'
+import MoneyInputFormatter from './money-input-formatter'
 
 export default function RootLayout({children}) {
-  return <html lang="vi"><body>{children}</body></html>
+  return <html lang="vi"><body><MoneyInputFormatter />{children}</body></html>
 }
